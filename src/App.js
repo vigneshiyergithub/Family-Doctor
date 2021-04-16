226798e6f3fd6c91bd09a7654ced6af35b910ad4
Trend20 <enockomondi305@gmail.com>
2021-04-16 15:04:43 +0300

    call commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Call from './components/Call/Call';
 import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Call />
     </div>
   );
 }
---
+++ src/components/Call/Call.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Call.css';
+
+function Call() {
+    return (
+        <div className="call-section">
+            <h3>High-class specialists are ready to help you at any time!</h3>
+            <p>Call us <a href="(123) 456-7891">(123) 456-7891</a></p>
+        </div>
+    )
+}
+
+export default Call;
