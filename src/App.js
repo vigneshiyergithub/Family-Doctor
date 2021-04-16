7c44d3029320a2adb58c7057c7fbd8f6f68d2db5
Trend20 <enockomondi305@gmail.com>
2021-04-16 15:37:08 +0300

    services commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Call from './components/Call/Call';
 import Hero from './components/Hero/Hero';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Call />
+      <Services />
     </div>
   );
 }
---
+++ src/components/Services/Services.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './Services.css';
+
+function Services() {
+    return (
+        <div className="services-section">
+            <h1>Our Services</h1>
+
+            <div className="services-gallery">
+                <div className="image">
+                    <img src="../../service1.png" alt="service"/>
+                </div>
+                <div className="image">
+                    <img src="../../service1.png" alt="service"/>
+                </div>
+                <div className="image">
+                    <img src="../../service1.png" alt="service"/>
+                </div>
+                <div className="image">
+                    <img src="../../service1.png" alt="service"/>
+                </div>
+                <div className="image">
+                    <img src="../../service1.png" alt="service"/>
+                </div>
+                <div className="image">
+                    <img src="../../service1.png" alt="service"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services;
