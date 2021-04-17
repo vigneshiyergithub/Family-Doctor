274eb4f9a399075f3ef383d0547095b192feb845
Trend20 <enockomondi305@gmail.com>
2021-04-17 23:42:18 +0300

    services commit

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -9,22 +9,22 @@ function Services() {
 
             <div className="services-gallery">
                 <div className="image">
-                    <img src="../../service1.png" alt="service"/>
+                    <img src="../../img/service1.jpg" alt="service"/>
                 </div>
                 <div className="image">
-                    <img src="../../service1.png" alt="service"/>
+                    <img src="../../img/service2.jpg" alt="service"/>
                 </div>
                 <div className="image">
-                    <img src="../../service1.png" alt="service"/>
+                    <img src="../../img/service3.jpg" alt="service"/>
                 </div>
                 <div className="image">
-                    <img src="../../service1.png" alt="service"/>
+                    <img src="../../img/service4.jpg" alt="service"/>
                 </div>
                 <div className="image">
-                    <img src="../../service1.png" alt="service"/>
+                    <img src="../../img/service5.jpg" alt="service"/>
                 </div>
                 <div className="image">
-                    <img src="../../service1.png" alt="service"/>
+                    <img src="../img/service6.jpg" alt="service"/>
                 </div>
             </div>
         </div>
