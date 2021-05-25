b3905e75d4e062d3da54f02c319e19aa584a9107
Trend20 <enockomondi305@gmail.com>
2021-05-25 15:29:06 +0300

    working on the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,6 +7,8 @@ function Modal({ handleClose, show, children }) {
 
 	const [name, setName] = useState('');
 	const [phone, setPhone] = useState('');
+	const [error, setError] = useState('You must fill all the Field');
+	const [success, setSuccess] = useState('We will contact you shortly!!');
 
 	// handling form submission
 	const handleSubmit = (event) => {
@@ -21,16 +23,30 @@ function Modal({ handleClose, show, children }) {
 	const handlePhoneChange = (event) => {
 		setPhone(event.target.value);
 	};
+
+	// form validation on button click
+
+	const showMessage = () => {
+		if (name !== '' && phone !== '') {
+			setSuccess(success);
+		} else {
+			setError(error);
+			console.log(error);
+		}
+	};
+
 	const showHideClassName = show ? 'modal display-block' : 'modal display-none';
 	return (
 		<div className={showHideClassName}>
 			<form className="modal-main" onSubmit={handleSubmit}>
 				<h3>Request A Callback</h3>
 				<p>We can call you in 30 seconds, just enter your number below</p>
-				{children}
+
 				<input type="text" placeholder="Name*" value={name} onChange={handleNameChange} />
 				<input type="text" placeholder="Phone*" value={phone} onChange={handlePhoneChange} />
-				<button type="submit">Request</button>
+				<button type="submit" onClick={showMessage}>
+					Request
+				</button>
 				<i onClick={handleClose}>
 					<AiFillCloseCircle />
 				</i>
