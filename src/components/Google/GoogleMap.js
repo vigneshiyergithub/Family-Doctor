9a726bf49ffa2b6a1b1885da25753cc7d1950ac7
vigneshiyergithub <vigneshiyer94@gmail.com>
2021-10-09 11:49:05 +0530

    Maps integration

--- src/components/Google/GoogleMap.js
+++ src/components/Google/GoogleMap.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
-import './GoogleMap.css';
+import "./GoogleMap.css";
 
 class GoogleMap extends Component {
-	render() {
-		const mapStyles = {
-			width: '100%',
-			height: '50%',
-			padding: '100px 0px',
-			marginBottom: '200px',
-		};
-		return (
-			<Map
-				google={this.props.google}
-				zoom={8}
-				style={mapStyles}
-				initialCenter={{ lat: -1.2920659, lng: 36.82194619999996 }}
-				id="map"
-			>
-				<Marker position={{ lat: -1.2920659, lng: 36.82194619999996 }} />
-			</Map>
-		);
-	}
+  render() {
+    return (
+		<div className="google-map-container">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.5419102911246!2d73.85385471469085!3d18.504397774498653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c012e5654f9b%3A0xc1336da78c19e40!2sSacchidanand%20Apartment!5e0!3m2!1sen!2sin!4v1633759899466!5m2!1sen!2sin"
+        width={"100%"}
+        height={"100%"}
+		title="maps"
+        style={{border:0}}
+        allowFullScreen=""
+        loading="lazy"
+      />
+	  </div>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-	apiKey: 'AIzaSyArlQAJp6OiOxgnoauwaUnseG0jXdL1bwQ',
+  apiKey: "AIzaSyArlQAJp6OiOxgnoauwaUnseG0jXdL1bwQ",
 })(GoogleMap);
